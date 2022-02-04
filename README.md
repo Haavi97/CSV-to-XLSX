bac268c8aeea514ca5f2aea3ad063bbae1066e08
Haavi <javierortincervera@gmail.com>
2022-02-04 22:43:41 +0200

    Added readme and cli commands

---
+++ README.md
@@ -0,0 +1,10 @@
+# CSV to XLSX
+> Simple converter for personal purposes. Could be much improved
+
+## Setup
+```bash
+pip install -r requirements.txt
+```
+
+## TODO!
+- [ ] Handle encodings. Typical headache
--- csv_to_xlsx.py
+++ csv_to_xlsx.py
@@ -1,6 +1,10 @@
+import os
 import csv
 import openpyxl
 import argh
+
+from tqdm import tqdm
+
 
 def csv_to_xlsx(csvfile):
     wb = openpyxl.Workbook()
@@ -13,5 +17,39 @@ def csv_to_xlsx(csvfile):
     wb.save(csvfile[:-4] + '.xlsx')
 
 
+def all(file='file_name or file_path'):
+    """
+    Converts all csv files in the folder. 
+    """
+    _csvs = tqdm(list_csv())
+    for _csv in _csvs:
+        _csvs.set_description('Converting: {:<20}'.format(_csv))
+        csv_to_xlsx(_csv)
+
+
+def file(file):
+    """Converts the given file"""
+    if valid_csv(file):
+        csv_to_xlsx(file)
+
+
+def show():
+    """Prints all the names of the csv files in the folder"""
+    print('csv files in the folder')
+    print('\n'.join(list_csv()))
+
+
+def list_csv():
+    return filter(lambda x: valid_csv(x), os.listdir())
+
+
+def valid_csv(s):
+    return s.split('.')[-1] == 'csv'
+
+
+parser = argh.ArghParser()
+parser.add_commands([all, show, file])
+
 if __name__ == '__main__':
-    csv_to_xlsx('export-address-token-0xd5b0524d734dd7f494c3e326534139129013b86d.csv')
+    parser.dispatch()
+    # argh.dispatch_command(main)
